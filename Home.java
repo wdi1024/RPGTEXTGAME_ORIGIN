a3acc3e81b37630e2f9454d77be10c633cd6a64f
Daein Weon <daein4667@gmail.com>
2023-05-15 17:05:03 +0900

    Inn 수정, Home 수정, heroStatus 수정

--- Home.java
+++ Home.java
@@ -1,6 +1,43 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Home {
 	int stageNum;
+	static heroStatus hero = new heroStatus();
+	static Scanner in = new Scanner(System.in);
+	static int boss_clear = 0;
+
+	static void selectMap() {
+		int map;
+		System.out.println("1.던전 \n2.상점 \n3.용병소 \n4.여관\n5.아이템 사용");
+		System.out.printf("장소 번호를 입력하세요. : ");
+		map = in.nextInt();
+		if (map == 1) {
+			SelectingDungeon.StartSelectingDungeon();
+		} else if (map == 2) {
+			Shop.StartShop();
+		} else if (map == 3) {
+			SoldierCamp.StartSoldierCamp();
+		} else if (map == 4) {
+			Inn.InnStart();
+		} else if (map == 5) {
+			ItemAtHome.StartItemAtHome();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("RPG TEXT GAME에 오신 것을 환영합니다.");
+		System.out.printf("우선 ");
+		heroStatus.setName();
+		heroStatus.selectJob();
+		hero.printStatus();
+		System.out.println("모험을 시작해 마을에 도착했습니다.");
+		System.out.println("==============================================");
+		while (boss_clear != 1)
+			selectMap();
+
+		System.out.println("보스를 물리쳐 게임을 클리어하셨습니다! 축하합니다!");
+	}
 
 }
--- heroStatus.java
+++ heroStatus.java
@@ -1,11 +1,67 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class heroStatus {
 	static String name, job;
-	static int selection, level, hp, maxHp, mp, maxMp, power, magic, hiredSoldierCnt, money, exp;
+	static int hp, maxHp, mp, maxMp, power, magic, selection = 0, hiredSoldierCnt = 0, money = 0, exp = 0, level = 1;
 	static ArrayList<Integer> solNum = new ArrayList<>();
+	static Scanner in = new Scanner(System.in);
+
+	static void setName() {
+		String good = "0";
+		while (!good.equals("y")) {
+			System.out.printf("캐릭터의 이름을 입력하세요 : ");
+			name = in.nextLine();
+			System.out.println("캐릭터의 이름으로 " + name + "을 사용하시겠습니까? y/n");
+			good = in.nextLine();
+		}
+		System.out.println("==============================================");
+	}
+
+	static void selectJob() {
+		while (selection < 1 || selection > 4) {
+			System.out.println("1.전사 \n2.궁수 \n3.마법사 \n4.도적 ");
+			System.out.printf("직업 번호를 선택하세요. : ");
+			selection = in.nextInt();
+			System.out.println("==============================================");
+			if (selection == 1) {
+				job = "전사";
+				hp = 50;
+				maxHp = 50;
+				mp = 20;
+				maxMp = 20;
+				power = 10;
+				magic = 2;
+			} else if (selection == 2) {
+				job = "궁수";
+				hp = 40;
+				maxHp = 40;
+				mp = 25;
+				maxMp = 25;
+				power = 7;
+				magic = 5;
+			} else if (selection == 3) {
+				job = "마법사";
+				hp = 30;
+				maxHp = 30;
+				mp = 50;
+				maxMp = 50;
+				power = 2;
+				magic = 10;
+			} else if (selection == 4) {
+				job = "도적";
+				hp = 40;
+				maxHp = 40;
+				mp = 25;
+				maxMp = 25;
+				power = 7;
+				magic = 5;
+			} else
+				System.out.println("직업 보기에 있는 번호를 선택하세요.");
+		}
+	}
 
 	void printStatus() {
 		System.out.println("이름 : " + name);
@@ -18,5 +74,6 @@ public class heroStatus {
 		System.out.println("동행 용병 수 : " + hiredSoldierCnt);
 		System.out.println("돈 : " + money);
 		System.out.println("경험치 : " + exp);
+		System.out.println("==============================================");
 	}
 }
