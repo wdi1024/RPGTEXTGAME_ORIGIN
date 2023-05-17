a4158334ec9a34018e79357fd0bffc5d871a6a82
juhwan0 <133099788+juhwan0@users.noreply.github.com>
2023-05-17 19:07:04 +0900

    return을 boolean으로 변경

--- Run.java
+++ Run.java
@@ -18,10 +18,15 @@ public class Run {
 			System.out.printf("\n헐레벌떡 도망치느라 hp가 1이 되었고 돈을 %d원 잃어버려습니다.", heroStatus.money * 25 / 100);
 			heroStatus.hp = 1;
 			heroStatus.money = heroStatus.money * 75 / 100;
-			break;// 홈으로 돌아감
+			return true;// 홈으로 돌아감
 		} else {
-			continue;// 전투 반복문 다시 시작
+			return false;
 		}
+	}
+
+	boolean canNotRun() {
+		System.out.print("보스에게서는 도망칠 수 없습니다!");
+		return false;
 	}
 
 }
