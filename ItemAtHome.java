d2e10cb9146b91f85648a51560c7264f45b43904
Daein Weon <daein4667@gmail.com>
2023-05-17 21:10:48 +0900

    ItemAtHome 포션 구현 추가, 상점 구매 시 돈 확인 기능 추

--- ItemAtHome.java
+++ ItemAtHome.java
@@ -15,10 +15,72 @@ public class ItemAtHome {
 			if (selectItem == 0)
 				break;
 			else if (selectItem <= (6 + MyInventory.skillbook.size()))
-				MyInventory.outItem(selectItem);
+				if (selectItem>6) {
+					HeroSkill.skillList.add(MyInventory.skillbook.get(selectItem-7));//임시
+					System.out.println("스킬 "+MyInventory.skillbook.get(selectItem-7)+"를 획득했습니다.");
+					MyInventory.outItem(selectItem);
+				}else if (selectItem==1) {
+					if (MyInventory.hpPotionCnt>0) {
+						System.out.println("체력이 50회복 되었습니다");
+						heroStatus.hp+=50;
+						if (heroStatus.hp>heroStatus.maxHp) {
+							heroStatus.hp=heroStatus.maxHp;
+						}
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("체력 회복포션 수가 부족합니다");
+					}
+					
+				}else if (selectItem==2) {
+					if (MyInventory.mpPotionCnt>0) {
+						System.out.println("마나가 50회복 되었습니다");
+						heroStatus.mp+=50;
+						if (heroStatus.mp>heroStatus.maxMp) {
+							heroStatus.mp=heroStatus.maxMp;
+						}
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("마나 회복포션 수가 부족합니다");
+					}
+				}else if (selectItem==3) {
+					if (MyInventory.maxHpPotionCnt>0) {
+						System.out.println("최대체력이 10증강 되었습니다");
+						heroStatus.maxHp+=10;
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("최대체력 증강포션 수가 부족합니다");
+					}
+				}else if (selectItem==4) {
+					if (MyInventory.maxMpPotionCnt>0) {
+						System.out.println("최대마나가 10증강 되었습니다");
+						heroStatus.maxMp+=10;
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("최대마나 증강포션 수가 부족합니다");
+					}
+				}else if (selectItem==5) {
+					if (MyInventory.powerPotionCnt>0) {
+						System.out.println("힘이 5증강 되었습니다");
+						heroStatus.power+=5;
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("힘 증강포션 수가 부족합니다");
+					}
+				}else if (selectItem==6) {
+					if (MyInventory.magicPotionCnt>0) {
+						System.out.println("마력이 5증강 되었습니다");
+						heroStatus.magic+=5;
+						MyInventory.outItem(selectItem);
+					}else {
+						System.out.println("마력 증강포션 수가 부족합니다");
+					}
+				}
+				
+			}
+
 			else
 				System.out.println("목록 내의 번호를 입력해주세요.");
 		}
-	}
+}
 
 }
--- Shop.java
+++ Shop.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-//안녕, dkdk
 
 public class Shop {
 	static int selectshop;
@@ -30,7 +29,12 @@ public class Shop {
 			else if (buyorsell == 1) {
 				while (true) {
 					System.out.println("0.돌아가기");
-					MyInventory.printInventory();
+					System.out.println("1.체력 회복포션 (10원)");
+					System.out.println("2.마나 회복포션 (10원)");
+					System.out.println("3.최대체력 증강포션 (100원)");
+					System.out.println("4.최대마나 증강포션: (100원)");
+					System.out.println("5.힘 증강포션 (50원)");
+					System.out.println("6.마력 증강 포션 (50원)");
 					System.out.println("=====================================");
 					System.out.printf("구매할 물품 번호를 입력하세요. :");
 					productnum = in.nextInt();
@@ -41,9 +45,64 @@ public class Shop {
 					System.out.printf("구매할 물건 개수를 입력하세요. :");
 					productcnt = in.nextInt();
 					System.out.println("=====================================");
-					if (checkMind()) {
-						MyInventory.addItem(productnum, productcnt);
+					if (productnum == 1) {
+						System.out.printf("가격은 %d입니다.\n", 10 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 10 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (10 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else if (productnum == 2) {
+						System.out.printf("가격은 %d입니다.\n", 10 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 10 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (10 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else if (productnum == 3) {
+						System.out.printf("가격은 %d입니다.\n", 100 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 100 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (100 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else if (productnum == 4) {
+						System.out.printf("가격은 %d입니다.\n", 100 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 100 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (100 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else if (productnum == 5) {
+						System.out.printf("가격은 %d입니다.\n", 50 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 50 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (50 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else if (productnum == 6) {
+						System.out.printf("가격은 %d입니다.\n", 50 * productcnt);
+						if (checkMind()) {
+							if (heroStatus.money >= 50 * productcnt) {
+								MyInventory.addItem(productnum, productcnt);
+								heroStatus.money -= (50 * productcnt);
+							} else
+								System.out.println("돈이 모자랍니다.");
+						}
+					} else {
+						System.out.println("목록 내의 번호를 입력해주십시오.");
 					}
+
 				}
 			} else if (buyorsell == 2) {
 				while (true) {
