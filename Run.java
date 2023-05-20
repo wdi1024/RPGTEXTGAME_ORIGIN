be8e6d403b8c039b51da697ad07a73f14ba22512
juhwa <juhwa@BOOK-54CIK34440>
2023-05-20 14:20:04 +0900

    canRun의 리턴 타입 boolean으로 변경, 구분선 추가

--- Run.java
+++ Run.java
@@ -10,10 +10,11 @@ public class Run {
 		}
 	}
 
-	void canRun() {
+	boolean canRun() {
 		Scanner in = new Scanner(System.in);
 		System.out.print("홈으로 도망가시겠습니까?(네:1 아니요:2 입력) : ");
 		int answer = in.nextInt();
+		System.out.println("=====================================");
 		if (answer == 1) {
 			System.out.printf("\n헐레벌떡 도망치느라 hp가 1이 되었고 돈을 %d원 잃어버려습니다.", heroStatus.money * 25 / 100);
 			heroStatus.hp = 1;
