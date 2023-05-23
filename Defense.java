a189698fde7c4e0a006f5e24670632a9164757b7
juhwan0 <133099788+juhwan0@users.noreply.github.com>
2023-05-23 09:39:28 +0900

    Monster 필드 용어 수정, 방어 계산식 변경

--- Defense.java
+++ Defense.java
@@ -14,12 +14,13 @@ public class Defense {// 아직 안 올림
 			if (selectMove == 0) {
 				break;
 			} else if (selectMove == 1) {
-				System.out.println("동료들과 방어진을 만들어 피해가 줄어듭니다");// 데미지 감소는 몬스터 공격력에서 플레이어 힘+마력 뺀 만큼
+				System.out.println("동료들과 방어진을 만들어 피해가 줄어듭니다");// 데미지 감소는 몬스터 공격력에서 플레이어와 동료 (힘+마력)/인원수 뺀 만큼
 				int sum = 0;
 				for (int i = 0; i < heroStatus.solNum.size(); i++) {
 					sum += heroStatus.solNum.get(i).solPower + heroStatus.solNum.get(i).solMagic;
 				}
-				HeroAttack.heroAttacked(Monster.monsterAttack(MonsPower - (heroStatus.power + heroStatus.magic + sum)));
+				HeroAttack.heroAttacked(Monster.monsterAttack(
+						Monster.MonsPower - (heroStatus.power + heroStatus.magic + sum) / heroStatus.solNum.size()));
 				break;
 			} else {
 				System.out.println("알맞은 번호를 입력해주세요.");
