2a8f11f63b488c5498e7b460bacd9fbe01fb5330
miniwa00@gmail.com <miniwa00@gmail.com>
2023-05-27 01:47:45 +0900

    오타 수정

--- solClass.java
+++ solClass.java
@@ -2,7 +2,6 @@ public class solClass {
 	String solName;
 	int solNum, solHp, solMaxHp, solMp, solMaxMp, solPower, solMagic;
 
-// 나는 바보다, 진짜
 	static void printSolStatus(solClass K) {
 		System.out.println("이름 : " + K.solName);
 		System.out.println("체력 : " + K.solHp + " 최대체력 : " + K.solMaxHp);
@@ -25,7 +24,6 @@ public class solClass {
 			solHp = heroStatus.level * 5 + 40;
 			solPower = heroStatus.level * 1 + 3;
 			solMagic = heroStatus.level * 5 + 10;
-			;
 			solNum = 2;
 		} else if (Num == 3) {// 힐 스킬 사용 가능
 			solName = "하급 성직자 안나";
@@ -33,7 +31,6 @@ public class solClass {
 			solHp = heroStatus.level * 5 + 40;
 			solPower = heroStatus.level * 1 + 3;
 			solMagic = heroStatus.level * 2 + 10;
-			;
 			solNum = 3;
 		} else if (Num == 4) {
 			solName = "중급 전사 제이드";
@@ -41,7 +38,6 @@ public class solClass {
 			solHp = heroStatus.level * 30 + 80;
 			solPower = heroStatus.level * 3 + 10;
 			solMagic = 0;
-			;
 			solNum = 4;
 		} else if (Num == 5) {
 			solName = "중급 마법사 케이토";
@@ -49,7 +45,6 @@ public class solClass {
 			solHp = heroStatus.level * 8 + 60;
 			solPower = heroStatus.level * 2 + 4;
 			solMagic = heroStatus.level * 7 + 15;
-			;
 			solNum = 5;
 		} else if (Num == 6) {
 			solName = "중급 성직자 메이";
@@ -57,7 +52,6 @@ public class solClass {
 			solHp = heroStatus.level * 8 + 60;
 			solPower = heroStatus.level * 2 + 4;
 			solMagic = heroStatus.level * 3 + 15;
-			;
 			solNum = 6;
 		} else if (Num == 7) {
 			solName = "상급 전사 알렉산더";
@@ -65,7 +59,6 @@ public class solClass {
 			solHp = heroStatus.level * 40 + 120;
 			solPower = heroStatus.level * 6 + 25;
 			solMagic = 0;
-			;
 			solNum = 7;
 		} else if (Num == 8) {
 			solName = "상급 마법사 간달프";
@@ -73,7 +66,6 @@ public class solClass {
 			solHp = heroStatus.level * 14 + 80;
 			solPower = heroStatus.level * 3 + 9;
 			solMagic = heroStatus.level * 10 + 25;
-			;
 			solNum = 8;
 		} else if (Num == 9) {
 			solName = "상급 성직자 마리아";
@@ -81,9 +73,7 @@ public class solClass {
 			solHp = heroStatus.level * 14 + 80;
 			solPower = heroStatus.level * 3 + 9;
 			solMagic = heroStatus.level * 6 + 20;
-			;
 			solNum = 9;
 		}
-
 	}
 }
