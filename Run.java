f4fd1dddd0fc9adc1152572008878b99be0ead6e
juhwan0 <133099788+juhwan0@users.noreply.github.com>
2023-05-27 11:59:44 +0900

    도망 시 딜레이 구현, 구분선 추가

--- Run.java
+++ Run.java
@@ -16,9 +16,20 @@ public class Run {
 		int answer = in.nextInt();
 		System.out.println("=====================================");
 		if (answer == 1) {
+			for (int i = 0; i < 3; i++) {
+				try {
+					Thread.sleep(500);
+					System.out.print(".");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			System.out.println("! ");
 			System.out.printf("헐레벌떡 도망치느라 hp가 1이 되었고 돈을 %d원 잃어버렸습니다.\n", heroStatus.money * 25 / 100);
 			heroStatus.hp = 1;
 			heroStatus.money = heroStatus.money * 75 / 100;
+			System.out.println("=====================================");
 			return true;// 홈으로 돌아감
 		} else {
 			return false;
