6c933cf4521bb12f5dd3f25dd89f066d50229638
miniwa00@gmail.com <miniwa00@gmail.com>
2023-05-27 22:05:35 +0900

    콜린, 안나 구현

--- SolSkill.java
+++ SolSkill.java
@@ -5,73 +5,64 @@ public class SolSkill {
 	static int solskill(int solnum) {
 		Random random = new Random();
 		int sum = 0;
+		// 용병 1
 		if (heroStatus.solNum.get(solnum).solNum == 1) {
-			int RAPercent = 40; // 리차드 어택 확률 (40%)
-			int RDPercent = 30; // 리차드 힐 확률 (30%)
-			int RHPercent = 30; // 리차드 강력어택 확률 (30%)
 			int randomNumber = random.nextInt(100) + 1;
 
-			if (randomNumber <= RAPercent) {
-				System.out.println("리차드 어택");
-				sum = heroStatus.solNum.get(solnum).solPower + 10;
-			} else if (RAPercent < randomNumber && randomNumber <= 70) {
-				System.out.println("리차드 힐");
-				heroStatus.solNum.get(solnum).solHp += 20;
-			} else if (randomNumber > 70) {
-				System.out.println("리차드 강력어택");
-				sum = heroStatus.solNum.get(solnum).solPower + 20;
-				// 전체 용병에게 적용
-				for (int i = 0; i < heroStatus.solNum.size(); i++)
-					sum += 20;
+			if (40 < randomNumber && randomNumber <= 70) {
+				System.out.println("리차드 강타 (힘 2배 증가");
+				sum = heroStatus.solNum.get(solnum).solPower * 50;
+			} else if (70 < randomNumber && randomNumber <= 90) {
+				System.out.println("리차드 무자비한 일격 (힘 4배 증가)");
+				sum = heroStatus.solNum.get(solnum).solPower * 80;
+			} else if (randomNumber > 90) {
+				System.out.println("리차드 심연의 부름 (최대 체력 10 증가)");
+				heroStatus.solNum.get(solnum).solMaxHp += 10;
+			} else {
+				System.out.println("리차드 베기");
+				sum = heroStatus.solNum.get(solnum).solPower;
 			}
 		}
-		return sum;
-//		if (solnum == 2) {
-//			System.out.println("리차드 어택");
-//			int RAPercent = 40; // 리차드 어택 확률 (40%)
-//            int RDPercent = 30; // 리차드 힐 확률 (30%)
-//            int RHPercent = 30; // 리차드 강력어택 확률 (30%)
-//            int randomNumber = random.nextInt(100) + 1;
-//            
-//            if (randomNumber < RAPercent) {
-//    			System.out.println("리차드 어택");
-//            	sum = solClass.solPower + 10;
-//            }           
-//            else if  (RAPercent < randomNumber && randomNumber < 70) {
-//    			System.out.println("리차드 힐");
-//            	sum = solClass.solHp + 20;
-//            }
-//            else if (randomNumber > 70) {
-//    			System.out.println("리차드 강력어택");
-//            	sum = solClass.solPower + 20;
-//            	// 전체 용병에게 적용 
-//            	for (int i =0; i<heroStatus.solNum.size(); i++)
-//            	sum = heroStatus.solNum.get(i).solPower + 20;
-//            }
-//		}
-//        if (solClass.solNum == 1) {
-//			System.out.println("리차드 어택");
-//			int RAPercent = 40; // 리차드 어택 확률 (40%)
-//            int RDPercent = 30; // 리차드 힐 확률 (30%)
-//            int RHPercent = 30; // 리차드 강력어택 확률 (30%)
-//            int randomNumber = random.nextInt(100) + 1;
-//            
-//            if (randomNumber < RAPercent) {
-//    			System.out.println("리차드 어택");
-//            	sum = solClass.solPower + 10;
-//            }           
-//            else if  (RAPercent < randomNumber && randomNumber < 70) {
-//    			System.out.println("리차드 힐");
-//            	sum = solClass.solHp + 20;
-//            }
-//            else if (randomNumber > 70) {
-//    			System.out.println("리차드 강력어택");
-//            	sum = solClass.solPower + 20;
-//            	// 전체 용병에게 적용 
-//            	for (int i =0; i<heroStatus.solNum.size(); i++)
-//            	sum = heroStatus.solNum.get(i).solPower + 20;
-//            }
-//		}
+		// 용병 2
+		if (heroStatus.solNum.get(solnum).solNum == 2) {
+			int randomNumber = random.nextInt(100) + 1;
 
-	}
-}
+			if (40 < randomNumber && randomNumber <= 70) {
+				System.out.println("콜린 마법 화살 (마력 2배 증가");
+				sum = heroStatus.solNum.get(solnum).solMagic * 50;
+			} else if (70 < randomNumber && randomNumber <= 90) {
+				System.out.println("콜린 마법 회복 (MaxHp 10 증가)");
+				heroStatus.solNum.get(solnum).solMaxHp += 10;
+			} else if (90 < randomNumber) {
+				System.out.println("콜린 마력 증폭 (콜린 마력 4배 증가)");
+				sum = heroStatus.solNum.get(solnum).solMagic * 80;
+			} else {
+				System.out.println("콜린 마법진");
+				sum = heroStatus.solNum.get(solnum).solPower;
+			}
+		}
+		// 용병 3
+		if (heroStatus.solNum.get(solnum).solNum == 2) {
+			int randomNumber = random.nextInt(100) + 1;
+
+			if (40 < randomNumber && randomNumber <= 70) {
+				System.out.println("안나 치유의 손길 (HP 10 증가");
+				sum = heroStatus.solNum.get(solnum).solHp + 10;
+			} else if (70 < randomNumber && randomNumber <= 90) {
+				System.out.println("안나 생명력 흡수 (입힌 데미지만큼 체력 회복)");
+				sum = heroStatus.solNum.get(solnum).solMagic;
+				heroStatus.solNum.get(solnum).solHp += sum;
+			} else if (90 < randomNumber) {
+				System.out.println("안나 대규모 치유 (전체 HP 30 증가, 안나는 추가 10 증가");
+				sum = heroStatus.solNum.get(solnum).solHp + 10;
+				// 전체 용병에게 적용
+				for (int i = 0; i < heroStatus.solNum.size(); i++)
+					heroStatus.solNum.get(i).solHp += 30;
+			} else {
+				System.out.println("안나 신의 수호");
+				sum = heroStatus.solNum.get(solnum).solPower;
+			}
+		}		
+		return sum;
+	}	
+}	
