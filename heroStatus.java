fad0418d0a82988a6df7f98c236e552e59a88cac
juhwan0 <133099788+juhwan0@users.noreply.github.com>
2023-05-29 13:15:29 +0900

    초기 소지금 100으로 수정

--- heroStatus.java
+++ heroStatus.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class heroStatus {
 	static String name, job;
-	static int hp, maxHp, mp, maxMp, power, magic, selection = 0, money = 100000, exp = 0, level = 1;
+	static int hp, maxHp, mp, maxMp, power, magic, selection = 0, money = 100, exp = 0, level = 1;
 	static ArrayList<solClass> solNum = new ArrayList<solClass>();
 	static Scanner in = new Scanner(System.in);
 
